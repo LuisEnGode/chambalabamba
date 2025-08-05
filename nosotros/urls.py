440fb756ffd2c2b1563f12b9f3cd9d654e82f9bb
LuisEnGode <luisengode@gmail.com>
2025-08-05 09:24:11 -0500

    feat: con formato para las paginas de la seccion nosotros en gobernanza, terrigorio , principios_valores

--- nosotros/urls.py
+++ nosotros/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('nuestro-camino/', views.nuestro_camino, name='nuestro_camino'),
+    path('gobernanza/', views.gobernanza, name='gobernanza'),
+    path('principios-valores/', views.principios_valores, name='principios-valores'),
+    path('territorio/', views.territorio, name='territorio'),
+
     path('pilares/bienestar/', views.pilar_bienestar, name='pilar_bienestar'),
     path('pilares/ecologia/', views.pilar_ecologia, name='pilar_ecologia'),
     path('pilares/economia/', views.pilar_economia, name='pilar_economia'),
