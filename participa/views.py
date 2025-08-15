16fc1dec494b2cedd4d31711890044ac67c26b38
LuisEnGode <luisengode@gmail.com>
2025-08-15 13:22:20 -0500

    feat(participa): agregadas las paginas para visitas guiadas y voluntariado

--- participa/views.py
+++ participa/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 def voluntariado(request):
     return render(request, 'participa/voluntariado.html')
 
+def visitas(request):
+    return render(request, 'participa/visitas-guiadas.html')
 
 
 
