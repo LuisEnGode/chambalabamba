a7efcd3552b3d1246175e79f837caacdde45f36e
LuisEnGode <luisengode@gmail.com>
2025-09-25 11:33:32 -0500

    creado template de blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path,include
 from blog import views
-from blog.views import  crear_post
+
 
 urlpatterns = [
 
-
     path('',views.blog,name="Blog"),
-    path('categoria/<int:categoria_id>/',views.categoria,name="Categoria"),
-    path('nuevo/', crear_post, name='crear_post'),
-    path('post/<int:id>/', views.detalle_post, name='detalle_post'),
+
 ]
