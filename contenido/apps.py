8bd9fa649538ad4a4f829ba268c892b789287671
LuisEnGode <luisengode@gmail.com>
2025-09-25 10:20:00 -0500

    feat(contenido): actualizado

--- contenido/apps.py
+++ contenido/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class ContenidoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contenido'
+    default_auto_field = "django.db.models.BigAutoField"
+    verbose_name = "Contenido-Home"  # ← lo que quieres ver en el admin
